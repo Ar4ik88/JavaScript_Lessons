88a1ac8c974f162102ca9b3384c4cffa54f1626d
Ar4ik88 <ar4i7@ukr.net>
2023-02-01 19:34:12 +0200

    Home Task JS - Lesson 1

---
+++ Lessons/Lesson_1/js/main.js
@@ -0,0 +1,38 @@
+let str_1 = 'hello';
+    str_2 = 'owu';
+    str_3 = 'com';
+    str_4 = 'ua';
+
+    num_1 = 1;
+    num_2 = 10;
+    num_3 = -999;
+    num_4 = 123;
+    num_5 = 3.14;
+    num_6 = 2.7;
+    num_7 = 16;
+
+    bool_1 = true;
+    bool_2 = false;
+
+    console.log(str_1, str_2, str_3, str_4);
+    console.log(num_1, num_2, num_3, num_4, num_5, num_6, num_7);
+    console.log(bool_1, bool_2);
+
+    // Second Part Task
+
+    let firstName = 'Artem';
+        middleName = 'Ivanovych';
+        lastName = 'Ozirnyi';
+        person = firstName + middleName + lastName;
+
+        console.log(person);
+
+    // Third Part
+
+    let a = 100;
+        b ='100';
+        c = true;
+
+        console.log(typeof a);
+        console.log(typeof b);
+        console.log(typeof c);
