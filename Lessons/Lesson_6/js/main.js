91b5b356c08c423105e74666ec31c39fd13f33f1
ar4ik88 <ar4i7@ukr.net>
2023-02-17 17:50:17 +0200

    Home Task JS - Lesson 6

---
+++ Lessons/Lesson_6/js/main.js
@@ -0,0 +1,24 @@
+/**
+ *  Знайти та вивести довижину настипних стрінгових значень: 'hello world', 'lorem ipsum', 'javascript is cool'
+ */
+
+    console.log('Length of "hello world" : ' + ('hello world').length + ' symbols');
+    console.log('Length of "lorem ipsum" : ' + ('lorem ipsum').length + ' symbols');
+    console.log('Length of "javascript is cool" : ' + ('javascript is cool').length + ' symbols');
+
+/**
+ *  Перевести до великого регістру наступні стрінгові значення 'hello world', 'lorem ipsum', 'javascript is cool'
+ */
+
+    console.log(('hello world').toLocaleUpperCase());
+    console.log(('lorem ipsum').toLocaleUpperCase());
+    console.log(('javascript is cool').toLocaleUpperCase());
+
+/**
+ *  Перевести до нижнього регістру наступні стрінгові значення 'hello world', 'lorem ipsum', 'javascript is cool'
+ */
+
+    console.log(('HELLO WORLD').toLocaleLowerCase());
+    console.log(('LOREM IPSUM').toLocaleLowerCase());
+    console.log(('JAVASCRIPT IS COOL').toLocaleLowerCase());
+
